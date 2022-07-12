4ce9e6692e522072371c46f6527229c7f0e41892
lefepa <lfpatino82@misena.edu.co>
2022-07-11 21:53:13 -0500

    hace la prueba pero no pasa test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    content: '',
+    
+  }
+
+  
+
+
+  
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" name="content" value = {this.state.content} onChange = {this.handleOnChange} > </textarea>
+        <div className="counter">{this.state.content.length}</div>
       </div>
     );
-  }
+  
 }
 
+handleOnChange = (e) => {
+  this.setState({[e.target.name]:e.target.value})
+}
+
+}
 export default App;
